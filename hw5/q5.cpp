2fd9aa16f9cfb5a419db5535441c08ca4d5fb88f
Gai Shaked <shaked.gai@gmail.com>
2011-01-06 13:28:45 +0200

    Question 5 - a, b

---
+++ hw5/q5.cpp
@@ -0,0 +1,20 @@
+
+void MySeries(char* name, double a0, int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9, int a10, int a11, int a12) {
+  return;
+}
+
+void MySeries(char* name, int a0, double a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9, int a10, int a11, int a12) {
+  return;
+}
+
+void MySeries(char* name, int a0, int a1, int a2, double a3, int a4, int a5, int a6, int a7, int a8, int a9, int a10, int a11, int a12) {
+  return;
+}
+
+void MySeries(char* name, int a0, int a1, int a2, int a3, double a4, int a5, int a6, int a7, int a8, int a9, int a10, int a11, int a12) {
+  return;
+}
+
+int main() {
+  MySeries("fibbonachi", 0, 0+1, 1+1, 1+2, 2+3, 8, 13, 21, 34, 55, 89, 144);
+}
