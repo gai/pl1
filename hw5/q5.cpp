9630d7d9b8008dca0be86c55f511d4e7a8bc6c46
Gai Shaked <shaked.gai@gmail.com>
2011-01-09 17:03:32 +0200

    q5 - full

--- hw5/q5.cpp
+++ hw5/q5.cpp
@@ -7,12 +7,16 @@ void MySeries(char* name, int a0, double a1, int a2, int a3, int a4, int a5, int
   return;
 }
 
-void MySeries(char* name, int a0, int a1, int a2, double a3, int a4, int a5, int a6, int a7, int a8, int a9, int a10, int a11, int a12) {
+void MySeries(char* name, int a0, int a1, int a2, int a3, int a4, int a5, int a6, int a7, double a8, int a9, int a10, int a11, int a12) {
   return;
 }
 
-void MySeries(char* name, int a0, int a1, int a2, int a3, double a4, int a5, int a6, int a7, int a8, int a9, int a10, int a11, int a12) {
+void MySeries(char* name, int a0, int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, double a9, int a10, int a11, int a12) {
   return;
+}
+
+double operator+(int a, int b) {
+  return 3.14;
 }
 
 int main() {
