9e1acf05a1d89b7a3c1d53b20df680f53c9293b3
omriguy <omri.guy@gmail.com>
2011-01-23 18:32:03 +0200

    part c complete

--- hw6/hw6.pl
+++ hw6/hw6.pl
@@ -20,6 +20,8 @@ p1(3).
 		tree(c,tree(f,nil,nil),tree(g,nil,nil))).
 *******************************************************************************/
 /* part #3 b */
+/* this predicate will return true iff it gets a binary tree or an empty one */
+/* if it gets a tree that's not empty, it will check it's two sons			 */
 binarytree(nil).
 binarytree(tree(_,T1,T2)) :- (binarytree(T1),binarytree(T2)).
 /* examples
@@ -27,6 +29,8 @@ binarytree(tree(_,T1,T2)) :- (binarytree(T1),binarytree(T2)).
 	binarytree(tree(1,tree(1,nil,nil),nil)).
 *******************************************************************************/
 /* part #3 c */
+/* this predicate will return true iff it gets a full binary tree			*/
+/* if it gets a tree that's not only a noun, it will check it's two sons	*/
 full(tree(_,nil,nil)) :- !.
 full(tree(_,T1,T2)) :- (full(T1),full(T2)).
 /* examples
@@ -35,11 +39,15 @@ full(tree(_,T1,T2)) :- (full(T1),full(T2)).
 	full(tree(1,tree(1,nil,nil),tree(1,nil,nil))).
 *******************************************************************************/
 /* part #3 e */
+/* this predicate will return al of the nouns that's in the tree inorder	*/
+/* the order is defined from left to right - 								*/
+/* 		the left son, then the noun and last will be the right son			*/
 getnodesinorder(tree(W,nil,nil),X) :- W=X, !.
 getnodesinorder(tree(W,T1,T2),X) :- getnodesinorder(T1,X) ; 
 									W=X ;
 									getnodesinorder(T2,X).
 /* examples
+	getnodesinorder(nil,X).
 	getnodesinorder(tree(1,tree(2,nil,nil),tree(3,nil,nil)),X).
 	getnodesinorder(tree(a,
 					tree(b,tree(d,nil,nil),tree(e,nil,nil)),
